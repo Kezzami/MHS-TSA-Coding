398b5afee835f74b75f7be06bc06e1d532a1e97f
kylecui5 <kcui@ucvts.org>
2019-03-17 22:13:44 -0400

    Added button

---
+++ TSA Code/Assets/Scripts/HomeScreenControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class HomeScreenControl : MonoBehaviour
+{
+    public Button headFaculty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button head = headFaculty.GetComponent<Button> ();
+
+        head.onClick.AddListener(ClickHead);
+    }
+    
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    public void ClickHead()
+    {
+        SceneManager.LoadScene("HeadFaculty");
+    }
+}
